0356408203b2af94d93f705a1add288ee40e3a2d
Alex Marchant <alex_marchie@outlook.com>
2018-10-23 20:36:07 +0100

    Initial Commit

---
+++ Kegal Timer/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Kegal Timer
+//
+//  Created by Alex Marchant on 23/10/2018.
+//  Copyright © 2018 Alex Marchant. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
