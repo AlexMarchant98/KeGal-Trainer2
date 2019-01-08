9ea799a5c0b0712bdd0dd1a1f716ec04229567b1
Alex Marchant <alexmarchant@Alexs-MacBook-Pro.local>
2019-01-08 20:45:56 +0000

    Track Workout Feature with working calendar and Cocoa Pods library

--- Kegal Timer/Infrastructure/CustomCell.swift
+++ Kegal Timer/Infrastructure/CustomCell.swift
@@ -7,7 +7,19 @@
 //
 
 import UIKit
+import JTAppleCalendar
 
 class CustomCell: JTAppleCell {
+    @IBOutlet var dateLabel: UILabel!
     
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.backgroundColor = .clear
+        self.contentView.addSubview(self.dateLabel)
+        self.dateLabel.textAlignment = .center
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
--- Kegal Timer/View Controllers/TrackWorkoutsViewController.swift
+++ Kegal Timer/View Controllers/TrackWorkoutsViewController.swift
@@ -8,14 +8,39 @@
 
 import UIKit
 import CoreData
+import JTAppleCalendar
 
 class TrackWorkoutsViewController: UIViewController {
+    
+    @IBOutlet weak var calendarView: JTAppleCalendarView!
+    
+    let formatter = DateFormatter()
     
     var container: NSPersistentContainer? = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        setupCalendarView()
+    }
+    
+    func setupCalendarView() {
+        calendarView.minimumLineSpacing = 0
+        calendarView.minimumInteritemSpacing = 0
+    }
+    
+    func handleCellTextColor(view: JTAppleCell?, cellState: CellState) {
+        guard let myCustomCell = view as? CustomCell else { return }
+        
+        if cellState.isSelected {
+            myCustomCell.dateLabel.textColor = .black
+        } else {
+            if cellState.dateBelongsTo == .thisMonth {
+                myCustomCell.dateLabel.textColor = .white
+            } else {
+                myCustomCell.dateLabel.textColor = .gray
+            }
+        }
     }
     
     func getContext() -> NSManagedObjectContext {
@@ -64,3 +89,43 @@ class TrackWorkoutsViewController: UIViewController {
 
 }
 
+extension TrackWorkoutsViewController: JTAppleCalendarViewDataSource {
+    func configureCalendar(_ calendar: JTAppleCalendarView) -> ConfigurationParameters {
+        formatter.dateFormat = "yyyy MM dd"
+        formatter.timeZone = Calendar.current.timeZone
+        formatter.locale = Calendar.current.locale
+        
+        let startDate = formatter.date(from: "2019 01 01")!
+        let endDate = formatter.date(from: "2019 12 31")!
+        
+        
+        let parameters = ConfigurationParameters(startDate: startDate, endDate: endDate)
+        return parameters
+    }
+}
+
+extension TrackWorkoutsViewController: JTAppleCalendarViewDelegate {
+    
+    func calendar(_ calendar: JTAppleCalendarView, willDisplay cell: JTAppleCell, forItemAt date: Date, cellState: CellState, indexPath: IndexPath) {
+        let myCustomCell = cell as! CustomCell
+        
+        myCustomCell.dateLabel.text = cellState.text
+        
+        handleCellTextColor(view: myCustomCell, cellState: cellState)
+    }
+    
+    
+    func calendar(_ calendar: JTAppleCalendarView, cellForItemAt date: Date, cellState: CellState, indexPath: IndexPath) -> JTAppleCell {
+        let cell = calendar.dequeueReusableJTAppleCell(withReuseIdentifier: "CustomCell", for: indexPath) as! CustomCell
+        
+        cell.dateLabel.text = cellState.text
+        
+        handleCellTextColor(view: cell, cellState: cellState)
+        
+        return cell
+    }
+    
+    func calendar(_ calendar: JTAppleCalendarView, didSelectDate date: Date, cell: JTAppleCell?, cellState: CellState) {
+        handleCellTextColor(view: cell, cellState: cellState)
+    }
+}
