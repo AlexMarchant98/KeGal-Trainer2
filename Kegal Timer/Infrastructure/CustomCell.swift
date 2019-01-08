5898619cf32cf759492eca7335f25280157a5bb2
Alex Marchant <alexmarchant@Alexs-MacBook-Pro.local>
2019-01-08 20:43:40 +0000

    Track Workout Feature with working calendar

---
+++ Kegal Timer/Infrastructure/CustomCell.swift
@@ -0,0 +1,13 @@
+//
+//  CustomCell.swift
+//  Kegal Timer
+//
+//  Created by Alex Marchant on 06/01/2019.
+//  Copyright © 2019 Alex Marchant. All rights reserved.
+//
+
+import UIKit
+
+class CustomCell: JTAppleCell {
+    
+}
